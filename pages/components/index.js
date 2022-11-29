53edbe5c32c374cd9087f41b2de07c1965d5fe78
Chris Collinsworth <cmcollin41@gmail.com>
2022-11-29 10:17:26 -0700

    cleaned up mobile component view

--- pages/components/index.js
+++ pages/components/index.js
@@ -20,24 +20,24 @@ export default function Storefronts() {
         </Head>
 
         <div className="w-full max-w-7xl mx-auto">
-          <div className="grid grid-cols-4 gap-4 border-b border-gray-200 pb-10">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-4 border-b border-gray-200 pb-10">
             <div className="col-span-1">
               <h2 className="font-semibold text-sm text-gray-50">Page Sections</h2>
             </div>
-            <div className="col-span-3 grid grid-cols-6 gap-4">
+            <div className="col-span-1 md:col-span-3 grid grid-cols-2 md:grid-cols-6 gap-4">
               <Link href="/components/general/heroes" passHref className="text-gray-50 col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
                 <div className="w-full h-full flex flex-col justify-end">
                   <h3 className="text-base font-semibold text-gray-50">Hero</h3>
                   <p className="text-xs font-light text-gray-50">3 Components</p>
                 </div>
               </Link>
-              <div className="col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
+              <div className="col-span-1 md:col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
                 <div className="w-full h-full flex flex-col justify-end">
                   <h3 className="text-base font-semibold text-gray-50">FAQ</h3>
                   <p className="text-xs font-light text-gray-50">3 Components</p>
                 </div>
               </div>
-              <div className="col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
+              <div className="col-span-1 md:col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
                 <div className="w-full h-full flex flex-col justify-end">
                   <h3 className="text-base font-semibold text-gray-50">Us vs Them</h3>
                   <p className="text-xs font-light text-gray-50">2 Components</p>
@@ -46,30 +46,30 @@ export default function Storefronts() {
             </div>
           </div>
 
-          <div className="grid grid-cols-4 gap-4 border-b border-gray-200 py-10">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-4 border-b border-gray-200 py-10">
             <div className="col-span-1">
               <h2 className="font-semibold text-sm text-gray-50">Product Sections</h2>
             </div>
-            <div className="col-span-3 grid grid-cols-6 gap-4">
+            <div className="col-span-1 md:col-span-3 grid grid-cols-2 md:grid-cols-6 gap-4">
               <div className="col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
                 <div className="w-full h-full flex flex-col justify-end">
                   <h3 className="text-base font-semibold text-gray-50">Media Carousel</h3>
                   <p className="text-xs font-light text-gray-50">3 Components</p>
                 </div>
               </div>
-              <Link href="/components/products/options" passHref className="col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
+              <Link href="/components/products/options" passHref className="col-span-1 md:col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
                 <div className="w-full h-full flex flex-col justify-end">
                   <h3 className="text-base font-semibold text-gray-50">Media Grid</h3>
                   <p className="text-xs font-light text-gray-50">1 Component</p>
                 </div>
               </Link>
-              <Link href="/components/products/options" passHref className="col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
+              <Link href="/components/products/options" passHref className="col-span-1 md:col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
                 <div className="w-full h-full flex flex-col justify-end">
                   <h3 className="text-base font-semibold text-gray-50">Product Options</h3>
                   <p className="text-xs font-light text-gray-50">3 Components</p>
                 </div>
               </Link>
-              <div className="col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
+              <div className="col-span-1 md:col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
                 <div className="w-full h-full flex flex-col justify-end">
                   <h3 className="text-base font-semibold text-gray-50">Reviews</h3>
                   <p className="text-xs font-light text-gray-50">2 Components</p>
@@ -79,11 +79,11 @@ export default function Storefronts() {
           </div>
 
 
-          <div className="grid grid-cols-4 gap-4 border-b border-gray-200 py-10">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-4 border-b border-gray-200 py-10">
             <div className="col-span-1">
               <h2 className="font-semibold text-sm text-gray-50">Collection Sections</h2>
             </div>
-            <div className="col-span-3 grid grid-cols-6 gap-4">
+            <div className="col-span-1 md:col-span-3 grid grid-cols-2 md:grid-cols-6 gap-4">
               <div className="col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
                 <div className="w-full h-full flex flex-col justify-end">
                   <h3 className="text-base font-semibold text-gray-50">Collection Carousel</h3>
@@ -93,18 +93,18 @@ export default function Storefronts() {
             </div>
           </div>
 
-          <div className="grid grid-cols-4 gap-4 border-b border-gray-200 py-10">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-4 border-b border-gray-200 py-10">
             <div className="col-span-1">
               <h2 className="font-semibold text-sm text-gray-50">Content Sections</h2>
             </div>
-            <div className="col-span-3 grid grid-cols-6 gap-4">
+            <div className="col-span-1 md:col-span-3 grid grid-cols-2 md:grid-cols-6 gap-4">
               <Link href="/components/products/options" passHref className="col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
                 <div className="w-full h-full flex flex-col justify-end">
                   <h3 className="text-base font-semibold text-gray-50">Content Grid</h3>
                   <p className="text-xs font-light text-gray-50">3 Components</p>
                 </div>
               </Link>
-              <Link href="/components/products/options" passHref className="col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
+              <Link href="/components/products/options" passHref className="col-span-1 md:col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
                 <div className="w-full h-full flex flex-col justify-end">
                   <h3 className="text-base font-semibold text-gray-50">Article</h3>
                   <p className="text-xs font-light text-gray-50">3 Components</p>
@@ -113,11 +113,11 @@ export default function Storefronts() {
             </div>
           </div>
 
-          <div className="grid grid-cols-4 gap-4 border-b border-gray-200 py-10">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-4 border-b border-gray-200 py-10">
             <div className="col-span-1">
               <h2 className="font-semibold text-sm text-gray-50">Checkout Sections</h2>
             </div>
-            <div className="col-span-3 grid grid-cols-6 gap-4">
+            <div className="col-span-1 md:col-span-3 grid grid-cols-2 md:grid-cols-6 gap-4">
               <Link href="/apps/rally" className="col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
                 <div className="w-full h-full flex flex-col justify-end">
                   <img src="/apps/rally.svg" className="w-16 h-16 invert" />
@@ -129,11 +129,11 @@ export default function Storefronts() {
             </div>
           </div>
 
-          <div className="grid grid-cols-4 gap-4 border-b border-gray-200 py-10">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-4 border-b border-gray-200 py-10">
             <div className="col-span-1">
               <h2 className="font-semibold text-sm text-gray-50">Portal Sections</h2>
             </div>
-            <div className="col-span-3 grid grid-cols-6 gap-4">
+            <div className="col-span-1 md:col-span-3 grid grid-cols-2 md:grid-cols-6 gap-4">
               <Link href="/apps/rally" className="col-span-2 w-full h-48 border border-gray-200 rounded-sm p-5 hover:cursor-pointer">
                 <div className="w-full h-full flex flex-col justify-end">
                   <img src="/apps/jshot-stacked.svg" className="w-16 h-16 invert" />
